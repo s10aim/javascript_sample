c51c04081257eaf92869392ff7f275b4b25427fd
s10aim <50735855+s10aim@users.noreply.github.com>
2020-12-02 21:49:49 +0900

    三項演算子

--- src/index.js
+++ src/index.js
@@ -192,3 +192,24 @@ const newNameArr = nameArr.map((name) => {
   }
 });
 console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// ある条件 ? 条件がtrueの時 : 条件がfalseの時
+const val = 1 > 0 ? "trueです" : "falseです";
+console.log(val);
+
+// numが数字の時
+const num = 1300;
+console.log(num.toLocaleString());
+
+const formattedNum =
+  typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+console.log(formattedNum);
+
+// 関数のreturnに三項演算子
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? "100を超えています!!" : "許容範囲内です";
+};
+console.log(checkSum(50, 60));
